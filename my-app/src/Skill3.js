33f5209fe80a67ed0368fa9b9ec1f56bb3c4d2c8
Dawson Schmitt <dischmitt@usf.edu>
2024-07-05 21:40:29 -0400

    Implement dynamic skills section using props.

---
+++ my-app/src/Skill3.js
@@ -0,0 +1,15 @@
+//Task 2, Skills Section: Develop a component that lists your skills.
+//Each skill could be displayed using a separate component that receives props for the skill name and proficiency level.
+import React from "react";
+
+function Skill3(props) {
+  return (
+    <div className="SkillItem">
+      <h3>{props.name}</h3>
+      <p>Proficiency: {props.level}</p>
+      <p>{props.text}</p>
+    </div>
+  );
+}
+
+export default Skill3;
